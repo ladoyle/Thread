e812ebda848ec9fe62a41ae9a68899b07c7957fa
Philip Yan <45404673+philwillchill@users.noreply.github.com>
2020-07-07 18:45:55 -0700

    Added twitter4j core dependency

--- src/main/java/com/thread/Thread/ThreadApplication.java
+++ src/main/java/com/thread/Thread/ThreadApplication.java
@@ -2,12 +2,15 @@ package com.thread.Thread;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import twitter4j.TwitterException;
 
 @SpringBootApplication
 public class ThreadApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThreadApplication.class, args);
+
+
 	}
 
 }
