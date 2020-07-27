5e59e1566dd93a76ee67a783443dcce26e282466
Nayeri Novasartians <nnovasar@uci.edu>
2020-07-27 14:17:23 -0700

    Working on search

--- src/main/java/com/thread/Thread/controllers/FacebookController.java
+++ src/main/java/com/thread/Thread/controllers/FacebookController.java
@@ -19,19 +19,19 @@ public class FacebookController extends Controller {
     private String permissions = "user_about_me";
     private String authURL = "https://287242839181671graph.facebook.com/oauth/authorize?type=user_agent&client_id="+appID
             +"&redirect_uri="+domain+"&scope="+permissions;
-    // This will be replaced with dynamic userAccessToken
-    private String userAccessToken = "EAAlbeYvpA10BACL9jbckq0RLFC3gSwfFACPNLFmtiZCyWDFrOTeRmEeyNJaWq6VkLGuvmxkg2LlxigZCi4LZCBZA1Mj8gXnxrfZBptfof7aKI8XZAbofNJWOEVJeRnkDAHPEnim9NmVMjcTCEkMHDRHiTxxHHIGTem78eFmE6z7bEXWaIxCAMoJSM7mC6us28UR1vPd0vgoZCFiyYIpgkXUPTAbpiRpYoZBY01OIhExOJwZDZD";
+    // This will be replaced with logged in  userAccessToken
+    private String userAccessToken = "EAAlbeYvpA10BAAVYxsRNpa8K3ex5exZC4ZBqVA8GG6d4DtMCbk1k0Czb3lemckXVdToKGPQBUL16YZAOFgf25j0DoJAdXYfvqNokQx8xTLvCNv0t9k197piZBmWxQvGzLlZBtZC5TKqFlU8744nZCW7ZA4hTZAI8efzsZC9ZAvP4ZAbvNe6QzG7gwdhDNVhEUQF0wLbtTJOycMibEJGQN5gsNdZBdpwaZCJL1NWc8pej8BnQiI1gZDZD";
 
 
     //Facebook userFB = null;
     // replace this v with ^ + initializing it in the login
-    Facebook userFB = new FacebookTemplate(userAccessToken); // is Thread right to pass
+    Facebook userFB = new FacebookTemplate(userAccessToken);
 
     public ResponseEntity login() {
         // TODO
-//        // This will be the last thing after successful login
-//        userFB = new FacebookTemplate(userAccessToken, "Thread"); // is Thread right to pass
-//        System.out.println("FB Login");
+        // This will be the last thing after successful login
+        userFB = new FacebookTemplate(userAccessToken, "Thread"); // is Thread right to pass
+        System.out.println("FB Login");
         return null;
     }
 
@@ -40,17 +40,9 @@ public class FacebookController extends Controller {
         assert userFB != null: "unset userFB";
 
         PagedList<Post> posts = null;
-        System.out.println("userFB is = " + userFB);
-        if(userFB == null) {
-            //some undefined behavior
-            System.out.println("userFB is null");
-            return null;
-        }
 
         try {
-            System.out.println("post will be = " + userFB.feedOperations().getHomeFeed());
             posts = userFB.feedOperations().getHomeFeed();
-
         }
         catch (org.springframework.social.ApiException ignored){
             System.out.println(ignored.getMessage());
@@ -83,6 +75,7 @@ public class FacebookController extends Controller {
         return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
+/*    temporary decision: no commenting
     @PostMapping("/comment")
     public ResponseEntity comment(@RequestBody FaceBookModel body) {
         assert userFB != null: "unset userFB";
@@ -97,7 +90,7 @@ public class FacebookController extends Controller {
         }
         return new ResponseEntity<>(commentID, HttpStatus.OK);
     }
-
+*/
 
     public ResponseEntity unshared(@RequestBody FaceBookModel body) {
         assert userFB != null: "unset userFB";
@@ -117,5 +110,4 @@ public class FacebookController extends Controller {
         }
         return new ResponseEntity<>(null,HttpStatus.OK);
     }
-
 }
