25c407379c6f843ec1ffff01e1c4d708f888ef5b
Sam Nguyen <samnguyen2020@gmail.com>
2020-08-12 13:38:30 -0700

    Twitter Login works properly
    Search endpoint works when user successfully authorizes Thread for Twitter

--- src/main/java/com/thread/Thread/controllers/TwitterController.java
+++ src/main/java/com/thread/Thread/controllers/TwitterController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import twitter4j.*;
+import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
 
 import javax.servlet.ServletException;
@@ -12,14 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/twitter")
 public class TwitterController extends Controller {
     private Twitter twitter;
 
-    /*
+
     @GetMapping("/login")
     public void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         twitter = TwitterFactory.getSingleton();
@@ -38,13 +41,28 @@ public class TwitterController extends Controller {
     }
 
     @GetMapping("/login/callback")
-    public void callback(HttpServletRequest request, HttpServletResponse response) throws ServletException{
-        Twitter twitter = (Twitter) request.getSession().getAttribute("twitter");
+    public ResponseEntity<Map> callback(HttpServletRequest request, HttpServletResponse response) throws ServletException{
+        twitter = (Twitter) request.getSession().getAttribute("twitter");
         RequestToken requestToken = (RequestToken) request.getSession().getAttribute("requestToken");
         String verifier = request.getParameter("oauth_verifier");
         try {
-            twitter.getOAuthAccessToken(requestToken, verifier);
+            AccessToken accessToken = twitter.getOAuthAccessToken(requestToken, verifier);
+
+            String screenName = accessToken.getScreenName();
+            String userIdString = String.valueOf(accessToken.getUserId());
+            String accessTokenString = accessToken.getToken();
+            String accessTokenSecretString = accessToken.getTokenSecret();
+
+            Map<String, String> obj = new HashMap<String, String>();
+            obj.put("screenName", screenName);
+            obj.put("userId", userIdString);
+            obj.put("accessToken", accessTokenString);
+            obj.put("accessTokenSecret", accessTokenSecretString);
+            System.out.println(obj);
+
+            twitter.setOAuthAccessToken(accessToken);
             request.getSession().removeAttribute("requestToken");
+            return new ResponseEntity<>(obj, HttpStatus.OK);
         } catch (TwitterException e) {
             throw new ServletException(e);
         }
@@ -56,6 +74,7 @@ public class TwitterController extends Controller {
         response.sendRedirect(request.getContextPath()+ "/");
     }
 
+    /*
     @GetMapping("/post")
     public ResponseEntity<Status> post(HttpServletRequest request, HttpServletResponse response) throws IOException {
         //Twitter4j Setup
@@ -64,7 +83,7 @@ public class TwitterController extends Controller {
 
         //Retweet status based on id given in request body
         try {
-            retweet = twitter.updateStatus("Testing Twitter API Signin #2");
+            retweet = twitter.updateStatus("Testing Twitter");
         } catch (TwitterException e) {
             e.printStackTrace();
         }
@@ -75,7 +94,6 @@ public class TwitterController extends Controller {
         response.sendRedirect(request.getContextPath()+ "/");
         return new ResponseEntity<>(retweet, HttpStatus.OK);
     }
-
     */
 
     @Override
