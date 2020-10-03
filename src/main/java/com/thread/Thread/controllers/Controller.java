26d340ca2dab9aca6eea9424d9e1796fe86d3b2d
Nayeri Novasartians <nnovasar@uci.edu>
2020-10-02 21:07:27 -0700

    Facebook Controller fully tested.

--- src/main/java/com/thread/Thread/controllers/Controller.java
+++ src/main/java/com/thread/Thread/controllers/Controller.java
@@ -1,12 +1,13 @@
 package com.thread.Thread.controllers;
 
 import com.thread.Thread.models.Model;
+import facebook4j.FacebookException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public abstract class Controller {
     @GetMapping("/search")
-    abstract ResponseEntity<?> search(String q);
+    abstract ResponseEntity<?> search(String q) throws FacebookException;
 
     ResponseEntity<?> share(Model body) { return null; }
     ResponseEntity<?> react(Model body) { return null; }
--- src/main/java/com/thread/Thread/controllers/FacebookController.java
+++ src/main/java/com/thread/Thread/controllers/FacebookController.java
@@ -1,134 +1,107 @@
 package com.thread.Thread.controllers;
+
 import com.thread.Thread.models.FaceBookModel;
+import facebook4j.*;
+import facebook4j.auth.AccessToken;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.social.connect.Connection;
-import org.springframework.social.facebook.api.Facebook;
-import org.springframework.social.facebook.api.PagedList;
-import org.springframework.social.facebook.api.Post;
-import org.springframework.social.facebook.api.User;
-import org.springframework.social.facebook.connect.FacebookConnectionFactory;
-import org.springframework.social.oauth2.AccessGrant;
-import org.springframework.social.oauth2.OAuth2Operations;
-import org.springframework.social.oauth2.OAuth2Parameters;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
+import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/Facebook")
-public class FacebookController extends Controller {
-
-    private final String appID = "";
-    private final String appSecret = "";
+@RequestMapping("/facebook")
+public class FacebookController extends Controller{
     private Facebook facebook;
-    private AccessGrant accessToken;
+    private static boolean isAppAuthorizationSet = false;
 
-    private final FacebookConnectionFactory connectionFactory =
-            new FacebookConnectionFactory(appID, appSecret);
+    @PostMapping("/login")
+    public boolean login(@RequestBody FaceBookModel body) {
+        System.out.println("FB Login");
 
-    @GetMapping("/login")
-    public String login() {
-        OAuth2Operations operations = connectionFactory.getOAuthOperations();
-        OAuth2Parameters params = new OAuth2Parameters();
+        facebook = FacebookFactory.getSingleton();
+        AccessToken accessToken;
+        AccessToken extendedToken = null;
+        boolean tokenSuccess = true;
 
-        params.setRedirectUri("http://localhost:8080/Facebook/login/auth");
-        params.setScope("email,public_profile");
-
-        String url = operations.buildAuthenticateUrl(params);
-        System.out.println("The URL is" + url);
-        return "redirect:" + url;
-    }
-
-    @RequestMapping("/login/auth")
-    public ModelAndView loginAuthenticate(@RequestParam("code") String authorizationCode) {
-        OAuth2Operations operations = connectionFactory.getOAuthOperations();
-        accessToken = operations.exchangeForAccess(
-                authorizationCode,
-                "http://localhost:8080/Facebook/login/auth",
-                null);
-
-        Connection<Facebook> connection = connectionFactory.createConnection(accessToken);
-        facebook = connection.getApi();
-        String[] fields = { "id", "email", "first_name", "last_name" };
-        User userProfile = facebook.fetchObject("me", User.class, fields);
-        ModelAndView model = new ModelAndView("details");
-        model.addObject("user", userProfile);
-        return model;
+        try {
+            accessToken = new AccessToken(body.getToken(), Long.parseLong(body.getTokenSecret()));
+            System.out.println(accessToken.toString());
+            String shortLivedToken = accessToken.getToken();
+            if(!isAppAuthorizationSet) {
+                facebook.setOAuthAppId(body.getKey(), body.getKeySecret());
+                isAppAuthorizationSet = true;
+            }
+            extendedToken = facebook.extendTokenExpiration(shortLivedToken);
+            facebook.setOAuthAccessToken(extendedToken);
+        }
+        catch (FacebookException e) {
+            e.printStackTrace();
+            tokenSuccess = false;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            tokenSuccess = false;
+        }
+        if(extendedToken != null)
+            System.out.println(extendedToken.toString());
+        else
+            System.out.println("empty token");
+        return tokenSuccess;
     }
 
     @Override
-    public ResponseEntity search(@RequestParam String q) {
-        PagedList<Post> posts = facebook.feedOperations().getFeed();
-
-        /*try {
-            posts = userFB.feedOperations().getHomeFeed();
+    public ResponseEntity<?> search(@RequestParam String query) {
+        System.out.println("FB Search");
+        query = query.toLowerCase();
+        ResponseList<Post> posts = null;
+        ArrayList<Post> results = new ArrayList<>();
+        try {
+             posts = facebook.getFeed();
         }
-        catch (org.springframework.social.ApiException ignored){
-            System.out.println(ignored.getMessage());
-          return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-        }*/
+        catch (FacebookException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if(posts != null) {
+            for (Post post : posts) {
+                String message = post.getMessage();
+                if (message != null && message.toLowerCase().contains(query) && !results.contains(post))
+                    results.add(post);
+            }
+        }
 
-        System.out.println(">>> "+ posts);
-        // search through posts and find the relevant ones
-        return new ResponseEntity<> (posts, HttpStatus.OK);
-
+        return results.isEmpty() ? new ResponseEntity<>(null, HttpStatus.NO_CONTENT)
+                : new ResponseEntity<>(results, HttpStatus.OK);
     }
 
-
-    public ResponseEntity share(FaceBookModel body) {
-        assert facebook != null: "unset userFB";
-
-        return null;
+    @PostMapping("/share")
+    public ResponseEntity<?> share(@RequestBody FaceBookModel body) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+    @DeleteMapping("/share")
+    public ResponseEntity<?> unshare(@RequestBody FaceBookModel body) {
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
     }
 
     @PostMapping("/react")
-    public ResponseEntity react(@RequestBody FaceBookModel body) {
-        //like, love, care, haha, wow, sad, angry
-        assert facebook != null: "unset userFB";
-        try {
-            facebook.likeOperations().like(String.valueOf(body.getId()));
-        }
-        catch (org.springframework.social.ApiException ignore) {
-            System.out.println(ignore.getMessage());
-            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(null, HttpStatus.OK);
-    }
-
-/*    temporary decision: no commenting
-    @PostMapping("/comment")
-    public ResponseEntity comment(@RequestBody FaceBookModel body) {
-        assert userFB != null: "unset userFB";
-        String commentID;
-        try{
-            commentID = userFB.commentOperations().addComment(
-                    String.valueOf(body.getId()), body.getMessage());
-        }
-        catch (org.springframework.social.ApiException ignore) {
-            System.out.println(ignore.getMessage());
-            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(commentID, HttpStatus.OK);
-    }
-*/
-
-    public ResponseEntity unshared(@RequestBody FaceBookModel body) {
-        assert facebook != null: "unset userFB";
-
-        return null;
+    public ResponseEntity<?> react(@RequestBody FaceBookModel body) {
+        System.out.println("FB React");
+        return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
     }
 
     @DeleteMapping("/react")
-    public ResponseEntity unreact(@RequestBody FaceBookModel body) {
-        assert facebook != null: "unset userFB";
-        try {
-            facebook.likeOperations().unlike(String.valueOf(body.getId()));
-        }
-        catch (org.springframework.social.ApiException ignore) {
-            System.out.println(ignore.getMessage());
-            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(null,HttpStatus.OK);
+    public ResponseEntity<?> unreact(@RequestBody FaceBookModel body) {
+        System.out.println("FB Unreact");
+        return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/comment")
+    public ResponseEntity<?> comment(@RequestBody FaceBookModel body) {
+        System.out.println("FB Comment");
+        return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
     }
 }
