2e6306fdbf21be70c91e39a1cfbcd5cff791cc23
ladoyle <54452841+ladoyle@users.noreply.github.com>
2020-11-28 21:42:24 -0800

    JRAW now works with the maven repositories

--- src/main/java/com/project/thread/controllers/RedditController.java
+++ src/main/java/com/project/thread/controllers/RedditController.java
@@ -1,15 +1,18 @@
 package com.project.thread.controllers;
 
+import net.dean.jraw.http.UserAgent;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-//import net.dean.jraw.*;
+
 
 @RestController
 @RequestMapping("/reddit")
 public class RedditController extends Controller {
-//    private static final UserAgent userAgent =
-//            new UserAgent("");
+    private static final UserAgent userAgent =
+            new UserAgent("");
+
+
 
     @Override
     ResponseEntity<?> search(String q) {
